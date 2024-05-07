e865c674a70751b6bc0faedaeb7948afec9f0857
kipkurui collins sang <kipkuruicollinssang@gmail.com>
2024-05-07 09:06:49 +0300

    first routes created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
+import {TaskDetailsComponent} from "./task-details/task-details.component";
+import {TaskListComponent} from "./task-list/task-list.component";
+import {TaskFormComponent} from "./task-form/task-form.component";
+import {NgModule} from "@angular/core";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: TaskListComponent },
+  { path: 'task/:id', component: TaskDetailsComponent },
+  { path: 'add-task', component: TaskFormComponent },
+  { path: 'edit-task/:id', component: TaskFormComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
